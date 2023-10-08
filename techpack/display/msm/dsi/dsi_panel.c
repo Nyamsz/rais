b5c25b525080a6e44b59e5b67199a8c94af62f6d
sources labs <jualan.online18@gmail.com>
2023-10-08 00:55:31 +0000

    test move_high_8b

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -716,10 +716,11 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 
 	if (panel->bl_config.bl_inverted_dbv)
 		bl_lvl = (((bl_lvl & 0xff) << 8) | (bl_lvl >> 8));
-	//still bug, 	
-	//if (panel->bl_config.bl_move_high_8b)
-	//	bl_lvl = bl_lvl >> 8;
-	
+
+	if (panel->bl_config.bl_move_high_8b)
+        bl_lvl = (((bl_lvl & 0xff000000) >> 4) | ((bl_lvl & 0x00ff0000) >> 12) | ((bl_lvl & 0x0000ff00) >> 20)  | ((bl_lvl & 0x000000ff) >> 28));
+		//bl_lvl = bl_lvl & 0xf >> 4
+
 	if (panel->bl_config.bl_dcs_subtype == 0xc2)
 		rc = dsi_panel_dcs_set_display_brightness_c2(dsi, bl_lvl);
 	else
